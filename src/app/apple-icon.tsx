d3c2a43e6d6d05b68b6365c24941cec8dbd72658
srmartinellid-droid <srmartinelli.d@gmail.com>
2026-09-22 23:55:39 -0300

    fix(seo): use stable public brand asset for generated metadata

--- src/app/apple-icon.tsx
+++ src/app/apple-icon.tsx
@@ -7,7 +7,7 @@ export const contentType = "image/png";
 export default function AppleIcon() {
   return new ImageResponse(
     <div style={{ width: "100%", height: "100%", display: "flex", alignItems: "center", justifyContent: "center", background: "#0B3A64", borderRadius: 36 }}>
-      <img src="https://www.nascimentoreformas.com.br/logo.png" width="132" height="132" style={{ objectFit: "contain" }} />
+      <img src="https://whgznodgurkuskmsejos.supabase.co/storage/v1/object/public/site-media/brand/2c5157fa-8537-430e-b192-04ee916f7c30.png" width="132" height="132" style={{ objectFit: "contain" }} />
     </div>,
     size
   );
